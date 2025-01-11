b1034c2c3af5c7608aaf41b8cc892518483ef65e
Vitor Mouro <vitor.mouro@outlook.com>
2025-01-11 19:00:50 -0300

    add copilot

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = 'Explorer' })
+vim.keymap.set("n", "<leader>nh", vim.cmd.nohls, { desc = 'No search highlight' })
