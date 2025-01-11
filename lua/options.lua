86a680d0111af22eec62bfb75d6e58c189be13d0
Vitor Mouro <vitor.mouro@outlook.com>
2025-01-11 16:23:02 -0300

    New config

---
+++ lua/options.lua
@@ -0,0 +1,32 @@
+vim.g.mapleader = " "
+
+vim.opt.clipboard = "unnamedplus"
+
+-- indentation
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+
+-- search
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- UI
+vim.opt.cursorline = true
+vim.opt.colorcolumn = "120"
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = "┊ ",
+    trail = "·",
+    extends = "»",
+    precedes = "«",
+    nbsp = "×"
+}
+
+vim.opt.number = true
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+vim.opt.termguicolors = true
