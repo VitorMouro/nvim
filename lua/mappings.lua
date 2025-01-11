9e895399ef0907ca5489987864d4b52aeeb3a701
Vitor Mouro <vitor.mouro@outlook.com>
2025-01-11 20:06:11 -0300

    New mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,2 +1,7 @@
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = 'Explorer' })
 vim.keymap.set("n", "<leader>nh", vim.cmd.nohls, { desc = 'No search highlight' })
+
+vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { desc = 'Exit terminal mode' })
+
+vim.keymap.set("n", "<leader>q", vim.cmd.q, { desc = 'Quit' })
+
