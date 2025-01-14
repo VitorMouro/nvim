1444bf0050615103824ea953e2dfc173860c921c
Vitor Mouro <vitor.mouro@outlook.com>
2025-01-14 13:42:59 -0300

    remove some keybinds for cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -107,44 +107,13 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+        ["<C-l>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item
       },
       sources = {
         { name = "nvim_lsp" },
         { name = "buffer" },
         { name = "path" },
         -- You can add more sources if you like
-      },
-      -- Optional formatting
-      formatting = {
-        format = function(entry, vim_item)
-          -- fancy icons and a name of kind
-          vim_item.menu = ({
-            nvim_lsp = "[LSP]",
-            luasnip  = "[LuaSnip]",
-            buffer   = "[Buffer]",
-            path     = "[Path]",
-          })[entry.source.name]
-          return vim_item
-        end,
       },
     })
 
